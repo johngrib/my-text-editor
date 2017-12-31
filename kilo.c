164107f6fbfcc411607ee35860942aa23a8f11c7
John Grib <johngrib@woowahan.com>
2017-12-31 20:18:30 +0900

    add getCursorPosition function

--- kilo.c
+++ kilo.c
@@ -75,6 +75,25 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+        return -1;
+    }
+    printf("\r\n");
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+    editorReadKey();
+
+    return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
@@ -83,8 +102,7 @@ int getWindowSize(int *rows, int *cols) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
             return -1;
         }
-        editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
 
     } else {
         *cols = ws.ws_col;
