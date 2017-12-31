30f0b542c95546372ef90f5b76e65a6b20d145c4
John Grib <johngrib@woowahan.com>
2017-12-31 20:44:54 +0900

    update getCursorPosition

--- kilo.c
+++ kilo.c
@@ -76,22 +76,32 @@ char editorReadKey() {
 }
 
 int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
     if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
         return -1;
     }
-    printf("\r\n");
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1) {
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+            break;
         }
+        if (buf[i] == 'R') {
+            break;
+        }
+        i++;
     }
-    editorReadKey();
+    buf[i] = '\0';
 
-    return -1;
+    if (buf[0] != '\x1b' || buf[1] != '[') {
+        return -1;
+    }
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+        return -1;
+    }
+
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
