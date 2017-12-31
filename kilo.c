78f40987d4cc2fcaf3e45f0daadac68d8687aacb
John Grib <johngrib@woowahan.com>
2017-12-31 11:07:06 +0900

    enable rawMode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,17 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);     // turn off echo
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
