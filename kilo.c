c15ed1af587dc01f3e3bd2244466e9606ca77600
John Grib <johngrib@woowahan.com>
2017-12-31 20:05:43 +0900

    add initEditor function

--- kilo.c
+++ kilo.c
@@ -15,6 +15,8 @@
 /*** data ***/
 
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -90,7 +92,7 @@ int getWindowSize(int *rows, int *cols) {
 
 void editorDrawRows() {
     int y;
-    for (y = 0; y < 24; y++) {
+    for (y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -120,9 +122,16 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+        die("getWindowSize");
+    }
+}
+
 int main() {
 
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
