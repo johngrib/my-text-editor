b8f9ebf375ea83578ebbe9fb671b5c47717d5da1
John Grib <johngrib@woowahan.com>
2018-01-01 16:14:54 +0900

    step 88: add editorRowCxToRx()

--- kilo.c
+++ kilo.c
@@ -202,6 +202,18 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx) {
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++) {
+        if (row->chars[j] == '\t') {
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        }
+        rx++;
+    }
+    return rx;
+}
+
 void editorUpdateRow(erow *row) {
     int tabs = 0;
     int j;
