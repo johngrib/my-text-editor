e345ef9e3e4f015323d9ecf4ab0bc12d95fc558b
John Grib <johngrib@woowahan.com>
2018-01-01 21:09:54 +0900

    step 126: editorPrompt()

--- kilo.c
+++ kilo.c
@@ -68,6 +68,7 @@ struct editorConfig E;
 /*** prototypes ***/
 
 void editorSetStatusMesage(const char *fmt, ...);
+void editorRefreshScreen();
 
 /*** terminal ***/
 
@@ -585,6 +586,34 @@ void editorSetStatusMesage(const char *fmt, ...) {
 }
 
 /*** input ***/
+
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMesage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMesage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorMoveCursor(int key) {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
