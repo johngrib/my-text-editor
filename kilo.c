2c6f76739da71955f77ff6e04269f41018252f33
John Grib <johngrib@woowahan.com>
2018-01-01 16:22:53 +0900

    step 91: move to the end of the line with END

--- kilo.c
+++ kilo.c
@@ -439,7 +439,9 @@ void editorProcessKeypress() {
             E.cx = 0;
             break;
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) {
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case PAGE_UP:
         case PAGE_DOWN:
