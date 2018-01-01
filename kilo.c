80dbe95cc0b2e405ceb6f88122fc4022e32bccdc
John Grib <johngrib@woowahan.com>
2018-01-01 20:39:42 +0900

    step 118: simple backspacing

--- kilo.c
+++ kilo.c
@@ -609,7 +609,10 @@ void editorProcessKeypress() {
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
-            /* TODO */
+            if (c == DEL_KEY) {
+                editorMoveCursor(ARROW_RIGHT);
+            }
+            editorDelChar();
             break;
 
         case PAGE_UP:
