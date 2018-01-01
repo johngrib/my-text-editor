dd8f27cec7c88429b56070815b2015ea7cc8484b
John Grib <johngrib@woowahan.com>
2018-01-01 20:36:11 +0900

    step 116: editorRowDelChar()

--- kilo.c
+++ kilo.c
@@ -283,6 +283,16 @@ void editorRowInsertChar(erow *row, int at, int c) {
     E.dirty++;
 }
 
+void editorRowDelChar(erow *row, int at) {
+    if (at < 0 || at >= row->size) {
+        return;
+    }
+    memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+    row->size--;
+    editorUpdateRow(row);
+    E.dirty++;
+}
+
 /*** editor operations ***/
 
 void editorInsertChar(int c) {
