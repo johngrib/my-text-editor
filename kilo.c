b5b4b739ee5777084fd36995b2e50514ca031805
John Grib <johngrib@woowahan.com>
2018-01-01 21:05:15 +0900

    step 125: the enter key

--- kilo.c
+++ kilo.c
@@ -632,7 +632,7 @@ void editorProcessKeypress() {
 
     switch (c) {
         case '\r':
-            /* TODO */
+            editorInsertNewline();
             break;
         case CTRL_KEY('q'):
             if (E.dirty && quit_times > 0) {
