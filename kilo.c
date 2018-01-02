6841c45ba67dbd9eaddb1bf93e14255f2d4489c7
John Grib <johngrib@woowahan.com>
2018-01-02 22:51:06 +0900

    step 160: show the current filetype in the status bar

--- kilo.c
+++ kilo.c
@@ -719,7 +719,7 @@ void editorDrawStatusBar(struct abuf *ab) {
     char status[80], rstatus[80];
 
     int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d", E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
 
     if (len > E.screencols) {
         len = E.screencols;
