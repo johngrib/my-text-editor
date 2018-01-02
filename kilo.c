160bceefb21c657649bb76e3d035b2161d88be1d
John Grib <johngrib@woowahan.com>
2018-01-02 23:01:46 +0900

    step 164: rehighlight the entire file after setting E.syntax in editorSelectSyntaxHighlight()

--- kilo.c
+++ kilo.c
@@ -305,6 +305,11 @@ void editorSelectSyntaxHighlight() {
             int is_ext = (s->filematch[i][0] == '.');
             if ((is_ext && ext && !strcmp(ext, s->filematch[i])) || (!is_ext && strstr(E.filename, s->filematch[i]))) {
                 E.syntax = s;
+
+                int filerow;
+                for (filerow = 0; filerow < E.numrows; filerow++) {
+                    editorUpdateSyntax(&E.row[filerow]);
+                }
                 return;
             }
             i++;
