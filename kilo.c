cd60e78f1887f6ee8dfc21233ac1433833164bc1
John Grib <johngrib@woowahan.com>
2018-01-02 22:49:18 +0900

    step 159: add a pointer to the current editorSyntax struct

--- kilo.c
+++ kilo.c
@@ -75,6 +75,7 @@ struct editorConfig {
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
+    struct editorSyntax *syntax;
     struct termios orig_termios;
 };
 
@@ -960,6 +961,7 @@ void initEditor() {
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
+    E.syntax = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
         die("getWindowSize");
