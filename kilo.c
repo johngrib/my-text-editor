6f22d3113212969ac5bb7d6c79dad3428fbb81eb
John Grib <johngrib@woowahan.com>
2018-01-02 22:14:48 +0900

    step 147: editorSyntaxToColor()

--- kilo.c
+++ kilo.c
@@ -235,6 +235,15 @@ void editorUpdateSyntax(erow *row) {
     }
 }
 
+int editorSyntaxToColor(int hl) {
+    switch (hl) {
+        case HL_NUMBER:
+            return 31;
+        default:
+            return 37;
+    }
+}
+
 /*** row operations ***/
 
 int editorRowCxToRx(erow *row, int cx) {
